920c08d2c8ea028715881025212222b0c4cd99e9
erlendjs <a@example.com>
2017-04-03 18:06:37 +0200

    Add some parallel stuff.

--- lab2/code-parallel/poisson.c
+++ lab2/code-parallel/poisson.c
@@ -7,12 +7,22 @@
  * NTNU, October 2000
  * Revised, October 2001
  * Revised by Eivind Fonn, February 2015
+ *
+ * Parallel version by Morgan Kristiansen and Erlend Sveen, 2017
  */
 
+///////////////////////////////////////////////////////////////////////////////
+// Includes
+///////////////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <mpi.h>
+#include <omp.h>
 
+///////////////////////////////////////////////////////////////////////////////
+// Definitions
+///////////////////////////////////////////////////////////////////////////////
 #define PI 3.14159265358979323846
 #define true 1
 #define false 0
@@ -20,26 +30,154 @@
 typedef double real;
 typedef int bool;
 
+///////////////////////////////////////////////////////////////////////////////
 // Function prototypes
+///////////////////////////////////////////////////////////////////////////////
 real *mk_1D_array(size_t n, bool zero);
 real **mk_2D_array(size_t n1, size_t n2, bool zero);
 void transpose(real **bt, real **b, size_t m);
 real rhs(real x, real y);
 
+///////////////////////////////////////////////////////////////////////////////
+// Fortran functions
+///////////////////////////////////////////////////////////////////////////////
 // Functions implemented in FORTRAN in fst.f and called from C.
 // The trailing underscore comes from a convention for symbol names, called name
 // mangling: if can differ with compilers.
 void fst_(real *v, int *n, real *w, int *nn);
 void fstinv_(real *v, int *n, real *w, int *nn);
 
-int main(int argc, char **argv)
+///////////////////////////////////////////////////////////////////////////////
+// Global variables
+///////////////////////////////////////////////////////////////////////////////
+// Size and rank
+int commsize;
+int myrank;
+
+// Number of threads per process
+int nthreads = 1;
+
+// These define this process working area
+int from;
+int to;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions
+///////////////////////////////////////////////////////////////////////////////
+void usage (char *appname)
 {
-    if (argc < 2) {
-        printf("Usage:\n");
-        printf("  poisson n\n\n");
-        printf("Arguments:\n");
-        printf("  n: the problem size (must be a power of 2)\n");
-    }
+	printf ("usage: mpirun -np <p> %s <n> <t>\n", appname);
+	printf ("where:\n");
+	printf (" <p> is a positive integer (number of processes)\n");
+	printf (" <n> is a power of two (problem size)\n");
+	printf (" <t> is a positive integer (number of threads per process)\n");
+}
+
+void initialize (int argc, char **argv)
+{
+	// MPI Init
+	MPI_Init (&argc, &argv);
+	MPI_Comm_size (MPI_COMM_WORLD, &commsize);
+	MPI_Comm_rank (MPI_COMM_WORLD, &myrank);
+
+	// Check number of arguments
+	if (argc != 3)
+	{
+		if (!myrank)
+			usage (argv[0]);
+		exit (0);
+	}
+
+	// Get problem size (n)
+	int n = atoi (argv[1]);
+
+	// Get number of threads (nthreads)
+	nthreads = atoi (argv[2]);
+
+	// Check problem size
+	if (((n & (n - 1)) != 0))
+	{
+		if (!myrank)
+		{
+			fprintf (stderr, "'%i' is not a power of two, try again with a different <p>\n", n);
+			usage (argv[0]);
+		}
+		exit (1);
+	}
+
+	// Check n
+	if (n <= 0)
+	{
+		if (!myrank)
+		{
+			fprintf (stderr, "'%i' is not a positive integer, try again with a different <n>\n", n);
+			usage (argv[0]);
+		}
+		exit (2);
+	}
+
+	// Check nthreads
+	if (nthreads <= 0)
+	{
+		if (!myrank)
+		{
+			fprintf (stderr, "'%i' is not a positive integer, try again with a different <t>\n", nthreads);
+			usage (argv[0]);
+		}
+		exit (3);
+	}
+}
+
+void definearea (int m)
+{
+	// Calculate the number of rows per process and the remainder
+	int div = m / commsize;
+	int rem = m % commsize;
+
+	// Calculate offset due to remainder in previous processes
+	int offset = myrank < rem ? myrank * 1 : rem * 1;
+
+	// Calculate an adder for the number of rows we need to do
+	int adder = myrank < rem ? 1 : 0;
+
+	// Calculate the start
+	from = myrank * div + offset;
+
+	// Calculate the end
+	to = from + div + adder;
+
+	// Debug
+#if 1
+	if (!myrank)
+		printf ("div %i rem %i m %i commsize %i\n", div, rem, m, commsize);
+
+	printf ("Process %i calculating from %i to %i offset %i adder %i\n", myrank, from, to, offset, adder);
+#endif
+}
+
+void findmax (real **b, int m)
+{
+	// Global max and my max
+	double u_max = 0.0;
+	double mymax = 0.0;
+
+	// Calculate
+	for (size_t i = from; i < to; i++)
+		for (size_t j = 0; j < m; j++)
+			mymax = mymax > b[i][j] ? mymax : b[i][j];
+
+	// Reduce max
+	MPI_Allreduce (&mymax, &u_max, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+	// Print max if we're process 0
+	if (!myrank)
+		printf ("u_max = %e\n", u_max);
+}
+
+int main (int argc, char **argv)
+{
+	// Prepare ourselves for parallel operation
+	initialize (argc, argv);
 
     /*
      *  The equation is solved on a 2D structured grid and homogeneous Dirichlet
@@ -52,13 +190,16 @@ int main(int argc, char **argv)
     int m = n - 1;
     real h = 1.0 / n;
 
+	// Define this process working area
+	definearea (m);
+
     /*
      * Grid points are generated with constant mesh size on both x- and y-axis.
      */
     real *grid = mk_1D_array(n+1, false);
-    for (size_t i = 0; i < n+1; i++) {
+
+    for (size_t i = 0; i < n+1; i++)
         grid[i] = i * h;
-    }
 
     /*
      * The diagonal of the eigenvalue matrix of T is set with the eigenvalues
@@ -66,9 +207,9 @@ int main(int argc, char **argv)
      * Note that the indexing starts from zero here, thus i+1.
      */
     real *diag = mk_1D_array(m, false);
-    for (size_t i = 0; i < m; i++) {
+
+    for (size_t i = 0; i < m; i++)
         diag[i] = 2.0 * (1.0 - cos((i+1) * PI / n));
-    }
 
     /*
      * Allocate the matrices b and bt which will be used for storing value of
@@ -99,11 +240,9 @@ int main(int argc, char **argv)
      * of freedom, so it excludes the boundary (bug fixed by petterjf 2017).
      * 
      */
-    for (size_t i = 0; i < m; i++) {
-        for (size_t j = 0; j < m; j++) {
+    for (size_t i = 0; i < m; i++)
+        for (size_t j = 0; j < m; j++)
             b[i][j] = h * h * rhs(grid[i+1], grid[j+1]);
-        }
-    }
 
     /*
      * Compute \tilde G^T = S^-1 * (S * G)^T (Chapter 9. page 101 step 1)
@@ -115,48 +254,34 @@ int main(int argc, char **argv)
      * In functions fst_ and fst_inv_ coefficients are written back to the input 
      * array (first argument) so that the initial values are overwritten.
      */
-    for (size_t i = 0; i < m; i++) {
+    for (size_t i = 0; i < m; i++)
         fst_(b[i], &n, z, &nn);
-    }
+
     transpose(bt, b, m);
-    for (size_t i = 0; i < m; i++) {
+
+    for (size_t i = 0; i < m; i++)
         fstinv_(bt[i], &n, z, &nn);
-    }
 
-    /*
-     * Solve Lambda * \tilde U = \tilde G (Chapter 9. page 101 step 2)
-     */
-    for (size_t i = 0; i < m; i++) {
-        for (size_t j = 0; j < m; j++) {
+	// Solve Lambda * \tilde U = \tilde G (Chapter 9. page 101 step 2)
+    for (size_t i = 0; i < m; i++)
+        for (size_t j = 0; j < m; j++)
             bt[i][j] = bt[i][j] / (diag[i] + diag[j]);
-        }
-    }
 
-    /*
-     * Compute U = S^-1 * (S * Utilde^T) (Chapter 9. page 101 step 3)
-     */
-    for (size_t i = 0; i < m; i++) {
+	// Compute U = S^-1 * (S * Utilde^T) (Chapter 9. page 101 step 3)
+    for (size_t i = 0; i < m; i++)
         fst_(bt[i], &n, z, &nn);
-    }
+
     transpose(b, bt, m);
-    for (size_t i = 0; i < m; i++) {
+
+    for (size_t i = 0; i < m; i++)
         fstinv_(b[i], &n, z, &nn);
-    }
 
-    /*
-     * Compute maximal value of solution for convergence analysis in L_\infty
-     * norm.
-     */
-    double u_max = 0.0;
-    for (size_t i = 0; i < m; i++) {
-        for (size_t j = 0; j < m; j++) {
-            u_max = u_max > b[i][j] ? u_max : b[i][j];
-        }
-    }
+    // Compute maximal value of solution for convergence analysis in L_\infty norm.
+    findmax (b, m);
 
-    printf("u_max = %e\n", u_max);
-
-    return 0;
+	// Done
+	MPI_Finalize ();
+	return 0;
 }
 
 /*
@@ -164,7 +289,8 @@ int main(int argc, char **argv)
  * Other functions can be defined to swtich between problem definitions.
  */
 
-real rhs(real x, real y) {
+real rhs (real x, real y)
+{
     return 2 * (y - y*y + x - x*x);
 }
 
@@ -174,13 +300,11 @@ real rhs(real x, real y) {
  * stored in the array to the block structure, using displacement arrays.
  */
 
-void transpose(real **bt, real **b, size_t m)
+void transpose (real **bt, real **b, size_t m)
 {
-    for (size_t i = 0; i < m; i++) {
-        for (size_t j = 0; j < m; j++) {
+    for (size_t i = 0; i < m; i++)
+        for (size_t j = 0; j < m; j++)
             bt[i][j] = b[j][i];
-        }
-    }
 }
 
 /*
@@ -188,12 +312,12 @@ void transpose(real **bt, real **b, size_t m)
  * The only thing to notice here is the use of calloc to zero the array.
  */
 
-real *mk_1D_array(size_t n, bool zero)
+real *mk_1D_array (size_t n, bool zero)
 {
-    if (zero) {
-        return (real *)calloc(n, sizeof(real));
-    }
-    return (real *)malloc(n * sizeof(real));
+    if (zero)
+        return (real*) calloc (n, sizeof(real));
+
+    return (real*) malloc (n * sizeof(real));
 }
 
 /*
@@ -205,22 +329,20 @@ real *mk_1D_array(size_t n, bool zero)
  * 3. pointers are set for each row to the address of first element.
  */
 
-real **mk_2D_array(size_t n1, size_t n2, bool zero)
+real **mk_2D_array (size_t n1, size_t n2, bool zero)
 {
     // 1
-    real **ret = (real **)malloc(n1 * sizeof(real *));
+    real **ret = (real**) malloc (n1 * sizeof(real*));
 
     // 2
-    if (zero) {
-        ret[0] = (real *)calloc(n1 * n2, sizeof(real));
-    }
-    else {
-        ret[0] = (real *)malloc(n1 * n2 * sizeof(real));
-    }
-    
+    if (zero)
+        ret[0] = (real*) calloc (n1 * n2, sizeof(real));
+    else
+        ret[0] = (real*) malloc (n1 * n2 * sizeof(real));
+
     // 3
-    for (size_t i = 1; i < n1; i++) {
+    for (size_t i = 1; i < n1; i++)
         ret[i] = ret[i-1] + n2;
-    }
+
     return ret;
 }
