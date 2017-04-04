9fdda7752254da0455eef6f9a91cda582b64c535
erlendjs <a@example.com>
2017-04-04 16:08:10 +0200

    Refactor transpose.

--- lab2/code-parallel/poisson.c
+++ lab2/code-parallel/poisson.c
@@ -21,6 +21,7 @@
 #include <omp.h>
 
 #include "list.h"
+#include "transpose.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Definitions
@@ -265,82 +266,6 @@ int main (int argc, char **argv)
 real rhs (real x, real y)
 {
     return 2 * (y - y*y + x - x*x);
-}
-
-/*
- * Write the transpose of b a matrix of R^(m*m) in bt.
- * In parallel the function MPI_Alltoallv is used to map directly the entries
- * stored in the array to the block structure, using displacement arrays.
- */
-
-void transpose (real **bt, real **b, size_t m)
-{
-#if 1
-	// Allocate stuff
-	int *sendcounts = (int*) malloc (commsize * sizeof (int));
-	int *recvcounts = (int*) malloc (commsize * sizeof (int));
-	int *senddispl  = (int*) malloc (commsize * sizeof (int));
-	int *recvdispl  = (int*) malloc (commsize * sizeof (int));
-	
-	// Init
-	for (int i = 0; i < commsize; i++)
-	{
-		// At this point, from and to describe the rows for this process
-		// Get n
-		int nrows = to - from;
-		
-		sendcounts[i] = nrows * m;
-		senddispl[i] = from * m;
-		
-		int nrowsrecv = gettorow(i) - getfromrow(i);
-		
-		recvcounts[i] = nrowsrecv * m;
-		recvdispl[i] = getfromrow(i) * m;
-		
-		if (!myrank) printf ("rank %i nrows %i sendcounts[i] %i senddispl[i] %i recvcounts[i] %i recvdispl[i] %i\n",
-			i, nrows, sendcounts[i], senddispl[i], recvcounts[i], recvdispl[i]);
-		
-	}
-	
-	// Send
-	//MPI_Alltoall (b[0], m/commsize, MPI_DOUBLE,
-	//	bt[0], m/commsize, MPI_DOUBLE, MPI_COMM_WORLD);
-	
-	/*void parallel_transpose_all(int id,double *lphi,double *tlphi,double *tempaux1, int m,int ln){
-        serial_transpose(lphi, tempaux1,ln,m);
-        MPI_Datatype    block_t;
-        MPI_Datatype    ub_block_t;   //upperbound row
-        int             bl[2];      //block length
-        MPI_Aint        dl[2];      //displacement location
-        MPI_Datatype    type[2]; 
-        MPI_Type_vector(ln, ln, m, MPI_DOUBLE, &block_t);
-        bl[0] = bl[1] = 1;
-        dl[0] = 0;
-        dl[1] = ln*sizeof(double);
-        type[0] = block_t;
-        type[1] = MPI_UB;
-        MPI_Type_struct(2, bl, dl, type, &ub_block_t);
-        MPI_Type_commit(&ub_block_t);
- 
-       MPI_Alltoall( tempaux1,1,ub_block_t,tlphi,(ln*ln),MPI_DOUBLE,MPI_COMM_WORLD);
-        MPI_Type_free(&ub_block_t);
-        MPI_Type_free(&block_t);
-}*/
-
-	// Free
-	free (sendcounts);
-	free (recvcounts);
-	free (senddispl);
-	free (recvdispl);
-	
-	MPI_Finalize ();
-	exit(0);
-#else
-	// Naive
-	for (size_t i = 0; i < m; i++)
-        for (size_t j = 0; j < m; j++)
-            bt[i][j] = b[j][i];
-#endif
 }
 
 /*
---
+++ lab2/code-parallel/transpose.h
@@ -0,0 +1,8 @@
+#ifndef TRANSPOSE_H
+#define TRANSPOSE_H
+
+void inittranspose (int rank, int m);
+void printtranspose (int rank, int m);
+void transpose (double **bt, double **b, size_t m);
+
+#endif
