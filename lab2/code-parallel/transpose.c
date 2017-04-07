06ed17a5c11e9506804060631534a27842e73e09
erlendjs <a@example.com>
2017-04-07 18:47:27 +0200

    Comment stuff and do some testing.

--- lab2/code-parallel/transpose.c
+++ lab2/code-parallel/transpose.c
@@ -51,31 +51,31 @@ void inittranspose (int rank, int m)
 	recvcounts = (int*) malloc (commsize * sizeof (int));
 	senddispl  = (int*) malloc (commsize * sizeof (int));
 	recvdispl  = (int*) malloc (commsize * sizeof (int));
-	
+
 	// Init each entry
 	for (int i = 0; i < commsize; i++)
 	{
 		// Get the number of rows to send
 		int nrows = gettorow(rank) - getfromrow(rank);
-		
+
 		// Set our amount and displacement. We send all our data to everyone
 		sendcounts[i] = nrows * m;
 		senddispl[i] = getfromrow(rank) * m;
-		
+
 		// Get number of rows to receive
 		int nrowsrecv = gettorow(i) - getfromrow(i);
-		
+
 		// Set amount and receive index
 		recvcounts[i] = nrowsrecv;
 		recvdispl[i] = getfromrow(i);
 	}
-	
+
 	// Create MPI data type for matrix columns
 	MPI_Type_vector (m, 1, m, MPI_DOUBLE, &matrixcolumn);
 	MPI_Type_commit (&matrixcolumn);
 	MPI_Type_create_resized (matrixcolumn, 0, sizeof(double), &matrixcolumntype);
 	MPI_Type_commit (&matrixcolumntype);
-	
+
 #if 0
 	//
 	int rowstosend = gettorow(rank) - getfromrow(rank);
@@ -132,13 +132,13 @@ void transpose (double **bt, double **b, size_t m)
 	for (int i = 0; i < commsize; i++)
 	{
 		int indexto = (myrank - i) % commsize;
-		
+
 		int offset = getfromrow (myrank) * m;
-		
+
 		offset += indexto;
-		
+
 		MPI_Status status;
-		
+
 		MPI_Sendrecv (&b[0][offset], 1, (getadder(i) ? sendtypelarge : sendtypesmall),
 			i, 0,
 			&b[0][offset], 1, (getadder(i) ? recvtypelarge : recvtypesmall),
@@ -170,12 +170,11 @@ void printtranspose (int rank, int m)
 	{
 		// Recalculate nrows as it is not stored explicitly
 		int nrows = gettorow(rank) - getfromrow(rank);
-		
+
 		// Print contents
 		printf ("from %i to rank %i nrows %i"
 				" sendcounts[i] %i senddispl[i] %i recvcounts[i] %i recvdispl[i] %i\n",
 			rank, i, nrows, sendcounts[i], senddispl[i], recvcounts[i], recvdispl[i]);
-		
 	}
 }
 
@@ -184,24 +183,17 @@ void printtestmatrix (int rank, double *c, int m)
 	if (rank != myrank)
 		return;
 
-	if (m == 3)
-	printf ("rank %i "
-			"\t%f %f %f\n"
-			"\t%f %f %f\n"
-			"\t%f %f %f\n",
-			rank,
-			c[0], c[1], c[2],
-			c[3], c[4], c[5],
-			c[6], c[7], c[8]);
-	else
-	printf ("rank %i "
-			"\t%f %f %f %f %f %f %f\n"
-			"\t%f %f %f %f %f %f %f\n"
-			"\t%f %f %f %f %f %f %f\n",
-			rank,
-			c[0], c[1], c[2], c[3], c[4], c[5], c[6],
-			c[7], c[8], c[9], c[10], c[11], c[12], c[13],
-			c[14], c[15], c[16], c[17], c[18], c[19], c[20]);
+	int index = 0;
+
+	for (int i = 0; i < m; i++)
+	{
+		printf ("rank %i", rank);
+
+		for (int ii = 0; ii < m; ii++)
+			printf (" %f", c[index++]);
+
+		printf ("\n");
+	}
 }
 
 void testtranspose ()
@@ -210,55 +202,56 @@ void testtranspose ()
 	if (commsize != 3)
 	{
 		if (!myrank)
-			printf ("incorrect commsize, must test with 3\n");
+			printf ("Incorrect number of processes, must test with 3\n");
+
 		MPI_Finalize ();
 		exit (0);
 	}
-	
-	// Test matrix size
+
+	// Declare test matrix size
 	int m = 7;
-	
+
 	// Rebuild lists for our test purposes
 	destroylists ();
 	buildlists (m, commsize);
-	
+
 	// Create a source and destination matrix
 	double *b = (double*) malloc (m * m * sizeof (double));
 	double *c = (double*) malloc (m * m * sizeof (double));
-	
+
 	// Initialize source matrix with something we recognize, and set destination to zero
 	for (int i = 0; i < m*m; i++)
 	{
 		b[i] = (myrank * 100) + i;
 		c[i] = 0;
 	}
-	
+
 	// Initialize Alltoallv arrays
 	inittranspose (myrank, m);
-	
+
 	// Print their contents for examination
 	printtranspose (myrank, m);
-	
+
 	// Do the actual transmission and transposition using a single mpi call
 	transpose (&c, &b, m);
-	
-	// Print the results from each process
+
+	// Print the results from each process. Correct order is not guaranteed but this kinda helps.
 	for (int i = 0; i < commsize; i++)
 	{
 		MPI_Barrier (MPI_COMM_WORLD);
 		printtestmatrix (i, c, m);
 	}
-	
+
 	// Free transpose arrays
 	deinittranspose ();
-	
+
 	// Free matrix arrays
 	free (c);
 	free (b);
-	
+
 	// Free lists
 	destroylists ();
-	
+
 	// Exit
 	MPI_Finalize ();
 	exit(0);
--- lab2/code-parallel/transpose.h
+++ lab2/code-parallel/transpose.h
@@ -19,20 +19,26 @@ void deinittranspose (void);
 void transpose (double **bt, double **b, size_t m);
 
 /*
- * For debug purposes: Print the contents of the arrays made by
- * inittranspose.
+ * For debug purposes:
+ * Print the contents of the arrays made by inittranspose.
  */
 void printtranspose (int rank, int m);
 
 /*
- * Print matrix of fixed size 3x3 in c, if rank equals the current
- * process' rank.
+ * For debug purposes:
+ * Print matrix of size m in c.
  */
 void printtestmatrix (int rank, double *c, int m);
 
 /*
  * Test the transpose stuff. Note that it will destroy and rebuild
  * lists as it sees fit and may not be used in production.
+ *
+ * This test hast to be run with 3 processes. It allocates an m=7
+ * matrix, initializes it with values unique to each process and
+ * tries to distribute/transpose it across all three processes.
+ * Each process then prints what it got to standard output for
+ * visual inspection.
  */
 void testtranspose (void);
 
