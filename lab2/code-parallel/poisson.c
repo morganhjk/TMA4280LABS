742c0664e67eaab0c1e811aad92764323b120881
erlendjs <a@example.com>
2017-04-07 15:39:44 +0200

    Fix tabbing.

--- lab2/code-parallel/poisson.c
+++ lab2/code-parallel/poisson.c
@@ -151,107 +151,107 @@ int main (int argc, char **argv)
 	// Prepare ourselves for parallel operation
 	initialize (argc, argv);
 
-    /*
-     *  The equation is solved on a 2D structured grid and homogeneous Dirichlet
-     *  conditions are applied on the boundary:
-     *  - the number of grid points in each direction is n+1,
-     *  - the number of degrees of freedom in each direction is m = n-1,
-     *  - the mesh size is constant h = 1/n.
-     */
-    int n = atoi(argv[1]);
-    int m = n - 1;
-    real h = 1.0 / n;
+	/*
+	 *  The equation is solved on a 2D structured grid and homogeneous Dirichlet
+	 *  conditions are applied on the boundary:
+	 *  - the number of grid points in each direction is n+1,
+	 *  - the number of degrees of freedom in each direction is m = n-1,
+	 *  - the mesh size is constant h = 1/n.
+	 */
+	int n = atoi(argv[1]);
+	int m = n - 1;
+	real h = 1.0 / n;
 
 	// Define this process working area
 	definearea (m);
 	buildlists (m, commsize);
 	inittranspose (myrank, m);
 
-    /*
-     * Grid points are generated with constant mesh size on both x- and y-axis.
-     */
-    real *grid = mk_1D_array(n+1, false);
+	/*
+	 * Grid points are generated with constant mesh size on both x- and y-axis.
+	 */
+	real *grid = mk_1D_array(n+1, false);
 
-    for (size_t i = 0; i < n+1; i++)
-        grid[i] = i * h;
+	for (size_t i = 0; i < n+1; i++)
+		grid[i] = i * h;
 
-    /*
-     * The diagonal of the eigenvalue matrix of T is set with the eigenvalues
-     * defined Chapter 9. page 93 of the Lecture Notes.
-     * Note that the indexing starts from zero here, thus i+1.
-     */
-    real *diag = mk_1D_array(m, false);
+	/*
+	 * The diagonal of the eigenvalue matrix of T is set with the eigenvalues
+	 * defined Chapter 9. page 93 of the Lecture Notes.
+	 * Note that the indexing starts from zero here, thus i+1.
+	 */
+	real *diag = mk_1D_array(m, false);
 
-    for (size_t i = 0; i < m; i++)
-        diag[i] = 2.0 * (1.0 - cos((i+1) * PI / n));
+	for (size_t i = 0; i < m; i++)
+		diag[i] = 2.0 * (1.0 - cos((i+1) * PI / n));
 
-    /*
-     * Allocate the matrices b and bt which will be used for storing value of
-     * G, \tilde G^T, \tilde U^T, U as described in Chapter 9. page 101.
-     */
-    real **b = mk_2D_array(m, m, false);
-    real **bt = mk_2D_array(m, m, false);
+	/*
+	 * Allocate the matrices b and bt which will be used for storing value of
+	 * G, \tilde G^T, \tilde U^T, U as described in Chapter 9. page 101.
+	 */
+	real **b = mk_2D_array(m, m, false);
+	real **bt = mk_2D_array(m, m, false);
 
-    /*
-     * This vector will holds coefficients of the Discrete Sine Transform (DST)
-     * but also of the Fast Fourier Transform used in the FORTRAN code.
-     * The storage size is set to nn = 4 * n, look at Chapter 9. pages 98-100:
-     * - Fourier coefficients are complex so storage is used for the real part
-     *   and the imaginary part.
-     * - Fourier coefficients are defined for j = [[ - (n-1), + (n-1) ]] while 
-     *   DST coefficients are defined for j [[ 0, n-1 ]].
-     * As explained in the Lecture notes coefficients for positive j are stored
-     * first.
-     * The array is allocated once and passed as arguments to avoid doings 
-     * reallocations at each function call.
-     */
-    int nn = 4 * n;
-    real *z = mk_1D_array(nn, false);
+	/*
+	 * This vector will holds coefficients of the Discrete Sine Transform (DST)
+	 * but also of the Fast Fourier Transform used in the FORTRAN code.
+	 * The storage size is set to nn = 4 * n, look at Chapter 9. pages 98-100:
+	 * - Fourier coefficients are complex so storage is used for the real part
+	 *   and the imaginary part.
+	 * - Fourier coefficients are defined for j = [[ - (n-1), + (n-1) ]] while 
+	 *   DST coefficients are defined for j [[ 0, n-1 ]].
+	 * As explained in the Lecture notes coefficients for positive j are stored
+	 * first.
+	 * The array is allocated once and passed as arguments to avoid doings 
+	 * reallocations at each function call.
+	 */
+	int nn = 4 * n;
+	real *z = mk_1D_array(nn, false);
 
-    /*
-     * Initialize the right hand side data for a given rhs function.
-     * Note that the right hand-side is set at nodes corresponding to degrees
-     * of freedom, so it excludes the boundary (bug fixed by petterjf 2017).
-     * 
-     */
-    for (size_t i = from; i < to; i++)
-        for (size_t j = 0; j < m; j++)
-            b[i][j] = h * h * rhs(grid[i+1], grid[j+1]);
+	/*
+	 * Initialize the right hand side data for a given rhs function.
+	 * Note that the right hand-side is set at nodes corresponding to degrees
+	 * of freedom, so it excludes the boundary (bug fixed by petterjf 2017).
+	 * 
+	 */
+	for (size_t i = from; i < to; i++)
+		for (size_t j = 0; j < m; j++)
+			b[i][j] = h * h * rhs(grid[i+1], grid[j+1]);
 
-    /*
-     * Compute \tilde G^T = S^-1 * (S * G)^T (Chapter 9. page 101 step 1)
-     * Instead of using two matrix-matrix products the Discrete Sine Transform
-     * (DST) is used.
-     * The DST code is implemented in FORTRAN in fsf.f and can be called from C.
-     * The array zz is used as storage for DST coefficients and internally for 
-     * FFT coefficients in fst_ and fstinv_.
-     * In functions fst_ and fst_inv_ coefficients are written back to the input 
-     * array (first argument) so that the initial values are overwritten.
-     */
-    for (size_t i = from; i < to; i++)
-        fst_(b[i], &n, z, &nn);
+	/*
+	 * Compute \tilde G^T = S^-1 * (S * G)^T (Chapter 9. page 101 step 1)
+	 * Instead of using two matrix-matrix products the Discrete Sine Transform
+	 * (DST) is used.
+	 * The DST code is implemented in FORTRAN in fsf.f and can be called from C.
+	 * The array zz is used as storage for DST coefficients and internally for 
+	 * FFT coefficients in fst_ and fstinv_.
+	 * In functions fst_ and fst_inv_ coefficients are written back to the input 
+	 * array (first argument) so that the initial values are overwritten.
+	 */
+	for (size_t i = from; i < to; i++)
+		fst_ (b[i], &n, z, &nn);
 
-    transpose(bt, b, m);
+	transpose (bt, b, m);
 
-    for (size_t i = 0; i < m; i++)
-        fstinv_(bt[i], &n, z, &nn);
+	for (size_t i = 0; i < m; i++)
+		fstinv_ (bt[i], &n, z, &nn);
 
 	// Solve Lambda * \tilde U = \tilde G (Chapter 9. page 101 step 2)
-    for (size_t i = from; i < to; i++)
-        for (size_t j = 0; j < m; j++)
-            bt[i][j] = bt[i][j] / (diag[i] + diag[j]);
+	for (size_t i = from; i < to; i++)
+		for (size_t j = 0; j < m; j++)
+			bt[i][j] = bt[i][j] / (diag[i] + diag[j]);
 
 	// Compute U = S^-1 * (S * Utilde^T) (Chapter 9. page 101 step 3)
-    for (size_t i = from; i < to; i++)
-        fst_(bt[i], &n, z, &nn);
+	for (size_t i = from; i < to; i++)
+		fst_ (bt[i], &n, z, &nn);
 
-    transpose(b, bt, m);
+	transpose (b, bt, m);
 
-    for (size_t i = from; i < to; i++)
-        fstinv_(b[i], &n, z, &nn);
+	for (size_t i = from; i < to; i++)
+		fstinv_ (b[i], &n, z, &nn);
 
-    // Compute maximal value of solution for convergence analysis in L_\infty norm.
-    findmax (b, m);
+	// Compute maximal value of solution for convergence analysis in L_\infty norm.
+	findmax (b, m);
 
 	// Done
 	deinittranspose ();
@@ -261,26 +261,26 @@ int main (int argc, char **argv)
 }
 
 /*
- * This function is used for initializing the right-hand side of the equation.
- * Other functions can be defined to swtich between problem definitions.
- */
+* This function is used for initializing the right-hand side of the equation.
+* Other functions can be defined to swtich between problem definitions.
+*/
 
 real rhs (real x, real y)
 {
-    return 2 * (y - y*y + x - x*x);
+	return 2 * (y - y*y + x - x*x);
 }
 
 /*
- * The allocation of a vectore of size n is done with just allocating an array.
- * The only thing to notice here is the use of calloc to zero the array.
- */
+* The allocation of a vectore of size n is done with just allocating an array.
+* The only thing to notice here is the use of calloc to zero the array.
+*/
 
 real *mk_1D_array (size_t n, bool zero)
 {
-    if (zero)
-        return (real*) calloc (n, sizeof(real));
+	if (zero)
+		return (real*) calloc (n, sizeof(real));
 
-    return (real*) malloc (n * sizeof(real));
+	return (real*) malloc (n * sizeof(real));
 }
 
 /*
@@ -294,18 +294,18 @@ real *mk_1D_array (size_t n, bool zero)
 
 real **mk_2D_array (size_t n1, size_t n2, bool zero)
 {
-    // 1
-    real **ret = (real**) malloc (n1 * sizeof(real*));
+	// 1
+	real **ret = (real**) malloc (n1 * sizeof(real*));
 
-    // 2
-    if (zero)
-        ret[0] = (real*) calloc (n1 * n2, sizeof(real));
-    else
-        ret[0] = (real*) malloc (n1 * n2 * sizeof(real));
+	// 2
+	if (zero)
+		ret[0] = (real*) calloc (n1 * n2, sizeof(real));
+	else
+		ret[0] = (real*) malloc (n1 * n2 * sizeof(real));
 
-    // 3
-    for (size_t i = 1; i < n1; i++)
-        ret[i] = ret[i-1] + n2;
+	// 3
+	for (size_t i = 1; i < n1; i++)
+		ret[i] = ret[i-1] + n2;
 
-    return ret;
+	return ret;
 }
