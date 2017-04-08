cd3cd7ae70969d362eeeb2c3599049a46d040e18
Morgan Kristiansen <morganhjk@gmail.com>
2017-04-08 18:05:44 +0200

    Update job scripts.

--- p2/jobs/job_p16-t01.sh
+++ p2/jobs/job_p16-t01.sh
@@ -2,7 +2,7 @@
 #PBS -N poisson_p16-t01
 #PBS -A imf_lille-tma4280
 #PBS -W group_list=imf_lille-tma4280
-#PBS -l walltime=00:02:00
+#PBS -l walltime=00:05:00
 #PBS -l select=2:ncpus=20:mpiprocs=8
 #PBS -m abe
 #PBS -M morganhk@stud.ntnu.no
@@ -18,5 +18,7 @@ nsizes=( 2048 4096 8192 16384 )
 # Run program for all sizes
 for nsize in "${nsizes[@]}"
 do
-	mpirun "$poisson" "$nsize" 1
+	echo "=========================="
+	time mpirun "$poisson" "$nsize" 1
+	echo "=========================="
 done
