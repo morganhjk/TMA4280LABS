82b5338a95fee3b97a1f3d8423f6891075070c65
Morgan Kristiansen <morganhjk@gmail.com>
2017-04-08 17:18:42 +0200

    Add job scripts for Lille.

---
+++ p2/jobs/job_p18-t02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -N poisson_p18-t02
+#PBS -A imf_lille-tma4280
+#PBS -W group_list=imf_lille-tma4280
+#PBS -l walltime=00:02:00
+#PBS -l select=2:ncpus=20:mpiprocs=9:ompthreads=2
+#PBS -m abe
+#PBS -M morganhk@stud.ntnu.no
+cd "$PBS_O_WORKDIR" || exit 1
+module load gcc openmpi
+
+# Define executable
+poisson="../build/poisson"
+
+# Define problem sizes
+nsizes=( 2048 4096 8192 16384 )
+
+# Run program for all sizes
+for nsize in "${nsizes[@]}"
+do
+	mpirun "$poisson" "$nsize" 2
+done
